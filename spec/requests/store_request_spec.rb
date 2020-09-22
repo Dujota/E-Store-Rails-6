f41b535a49a80e2d3569d6cc299933be46c41016
Denis <ddujota@internationalsafety.com>
2020-09-22 18:19:38 -0400

    updated the req specs to also test what the html is in the response for render index

--- spec/requests/store_request_spec.rb
+++ spec/requests/store_request_spec.rb
@@ -1,27 +1,26 @@
 require "rails_helper"
 
 RSpec.describe "Stores", type: :request do
+  before :each do
+    @product = create(:product)
+    @product2 = create(:product)
+    @product3 = create(:product)
+  end
+
   describe "GET /index" do
     it "returns http success" do
       get store_index_url
       expect(response).to have_http_status(:success)
-
-      # Req spec dont test ui
-      # assert_select "nav.side_nav a", minimum: 4
-      # assert_select "main ul.catalog li", 3
-      # assert_select "h2", "Programming Ruby 1.9"
-      # assert_select ".price", /\$[,\d]+\.\d\d/
     end
 
     it "renders :index" do
       get store_index_url
-      expect(response).to render_template :new
+      expect(response).to render_template :index
 
-      # Req spec dont test ui
-      # assert_select "nav.side_nav a", minimum: 4
-      # assert_select "main ul.catalog li", 3
-      # assert_select "h2", "Programming Ruby 1.9"
-      # assert_select ".price", /\$[,\d]+\.\d\d/
+      assert_select "nav.side_nav a", minimum: 4
+      assert_select "main ul.catalog li", 3
+      assert_select "h2", "#{@product.title}"
+      assert_select ".price", /\$[,\d]+\.\d\d/
     end
   end
 end
