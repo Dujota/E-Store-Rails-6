f7245f3fe64d7ae232917e12ee06b1284e622562
Denis <ddujota@internationalsafety.com>
2020-09-25 10:54:12 -0400

    added a simple impression counter to the page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+  include ImpressionCounter
+
+  before_action :set_counter, only: [:index]
+
   def index
     @products = Product.order(:title)
+    @counter = session[:counter]
   end
 end
