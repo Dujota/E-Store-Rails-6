c6d64f3b2310f5fa4f5ca4dc417578a51ee641c2
Denis <ddujota@internationalsafety.com>
2020-10-06 17:35:27 -0400

    enum for the payment type allow null for cart id and order id

--- db/migrate/20201006212631_add_order_to_line_item.rb
+++ db/migrate/20201006212631_add_order_to_line_item.rb
@@ -1,5 +1,7 @@
 class AddOrderToLineItem < ActiveRecord::Migration[6.0]
   def change
-    add_reference :line_items, :order, null: false, foreign_key: true
+    # Allow null for cart id and order
+    add_reference :line_items, :order, null: true, foreign_key: true
+    change_column :line_items, :cart_id, :integer, null: true
   end
 end
